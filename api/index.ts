ec926b71c693d16c9e5a5fb01343389de5296479
bbonfim-taller <bruno4000@gmail.com>
2026-06-25 17:14:48 -0300

    CORS fix issue.

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,3 @@
-import express from 'express';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import app from '../server';
 
-const app = express();
-app.use(express.json());
-
-// ... [Insert your existing cache types, fetchCardSets(), /api/sets, and /api/scrape endpoint logic here] ...
-
-// DO NOT call app.listen() when deploying on Vercel as a Serverless Function!
-// Instead, export the express application:
 export default app;
